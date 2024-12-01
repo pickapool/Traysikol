1d36882b071ea91ff38ed82cdbeea27e9a091c06
Cyrille Con Morales <cyrilleconm@gmail.com>
2024-12-02 00:31:46 +0800

    Fix orientation

--- app/src/main/java/com/example/traysikol/Passenger/PassengerProfileDetails.java
+++ app/src/main/java/com/example/traysikol/Passenger/PassengerProfileDetails.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.exifinterface.media.ExifInterface;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -41,6 +45,8 @@ import com.squareup.picasso.Picasso;
 import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Objects;
@@ -129,23 +135,47 @@ public class PassengerProfileDetails extends AppCompatActivity {
                     Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
-        } else if(requestCode == 1223 && resultCode == RESULT_OK) {
+        } else if (requestCode == 1223 && resultCode == RESULT_OK) {
             try {
                 Uri selectedImageUri = data.getData();
                 Picasso.get().load(selectedImageUri).into(pp);
                 Extensions.UploadProfilePicture(PassengerProfileDetails.this, selectedImageUri);
-            } catch (Exception ee)
-            {
-                System.out.println("33kkk"+ee.getMessage());
+            } catch (Exception ee) {
+                System.out.println("33kkk" + ee.getMessage());
             }
-        } else if(requestCode == 1225 && resultCode == RESULT_OK) {
-            File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"capture.jpg");
+        } else if (requestCode == 1225 && resultCode == RESULT_OK) {
+            File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "capture.jpg");
             Uri uri = FileProvider.getUriForFile(Objects.requireNonNull(getApplicationContext()),
                     BuildConfig.APPLICATION_ID + ".provider", file);
-            if(uri != null)
-            {
-                Picasso.get().load(uri).into(pp);
-                Extensions.UploadProfilePicture(PassengerProfileDetails.this, uri);
+
+            try {
+                ExifInterface exif = new ExifInterface(file.getAbsolutePath());
+                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+
+                Matrix matrix = new Matrix();
+                if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                    matrix.postRotate(90);
+                } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                    matrix.postRotate(180);
+                } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                    matrix.postRotate(270);
+                }
+
+                // Apply rotation to the bitmap
+                Bitmap originalBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                Bitmap rotatedBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, originalBitmap.getWidth(), originalBitmap.getHeight(), matrix, true);
+
+                // Save the rotated bitmap to a temporary file (or replace the original if needed)
+                FileOutputStream out = new FileOutputStream(file);
+                rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                out.flush();
+                out.close();
+
+                // Now load the rotated image into ImageView using Picasso
+                Picasso.get().load(file).into(pp);
+                Extensions.UploadProfilePicture(PassengerProfileDetails.this, Uri.fromFile(file));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
